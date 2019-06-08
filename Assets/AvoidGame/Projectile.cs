9e2644ffdf8279242643f6fe112bb6702145b507
Jake Overall <joverall22@gmail.com>
2019-06-08 16:39:19 -0600

    end of day 1

---
+++ Assets/AvoidGame/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float Speed = 40;
+    Vector2 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target.x = 20;
+        target.y = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            target,
+            Time.deltaTime * Speed
+         );
+        if(transform.position.x >= 20)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/ClickerGame/UIUpdater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIUpdater : MonoBehaviour
+{
+
+    public Clicker Clicker;
+    TextMeshProUGUI textContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textContainer = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update the text value to the clicker value
+        textContainer.text = $"YOU'VE CLICKED IT {Clicker.ClickCount} NUMBER OF TIMES";
+    }
+}
